c2090c31b4f460633f511d976fea2ab900be7bc2
Caio Felix <199407954+Caio-Felix1@users.noreply.github.com>
2025-07-10 17:31:44 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,7 +35,12 @@ public class Program {
 		
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
 		
+		System.out.println("Update completed");
 	}
 
 }
